ca45b2638f48c5258654ecb809fecdbb190fcf22
Ronak <130481625+Ronak1257@users.noreply.github.com>
2023-08-23 23:15:09 +0530

    Update Crowdfunding.sol

--- Crowdfunding.sol
+++ Crowdfunding.sol
@@ -1,27 +1,37 @@
+// SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.3;
-contract SolidityFundamentals2{
+
+contract crowdfunding{
     enum State{
         IN_PROGRESS,
         ENDED
     }
+
     address payable public owner;
     State public currentState;
+
     constructor(){
         owner=payable (msg.sender);
     }
+
+    //check state of the funding
     modifier stillInProgress(){
         require(currentState==State.IN_PROGRESS,"donation phase is no longer in progress");
         _;
     }
+
+    //donate any user address
     function donate() external payable stillInProgress{}
-    
+
+    // check total collected amount
     function checkAmountCollected() public view stillInProgress returns (uint){
         return address(this).balance;
     }
+
+    //withdraw only done by owner address
     function withdraw() external{
         require(msg.sender==owner,"only the owner can withdrew");
         owner.transfer(address(this).balance);
         currentState=State.ENDED;
     }
-
 }
